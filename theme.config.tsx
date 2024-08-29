834ae4fb6ec387f482a32c32091d03bc7a7997ac
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-08-29 16:21:02 -0400

    docs: add docs example

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: "DropDash",
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/tyronejosee/project_drop_dash_api",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://github.com/tyronejosee/project_drop_dash_docs/tree/main/",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "2024 - DropDash - all right reserved.",
   },
-}
+};
 
-export default config
+export default config;
